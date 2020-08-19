312c03dd19c080497b99fa801c411c9bddc11631
Zeye Gu <zeyegu@gmail.com>
2020-08-19 16:50:22 -0400

    purchase

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -4,7 +4,7 @@ import BuildControl from "./BuildControl/BuildControl";
 
 const controls =[
     {label: 'Salad', type:'salad'},
-    {label: 'Bacon', type:'Bacon'},
+    {label: 'Bacon', type:'bacon'},
     {label: 'Cheese', type:'cheese'},
     {label: 'Meat', type:'meat'},
 
@@ -12,9 +12,18 @@ const controls =[
 
 const BuildControls = (props) => (
     <div className={classes.BuildControls}>
+        <p>Current Price:<strong>{props.price.toFixed(2)}</strong></p>
         {controls.map(ctrl => (
-            <BuildControl key={ctrl.label} label={ctrl.label}/>
+            <BuildControl
+                key={ctrl.label}
+                label={ctrl.label}
+                added={()=>props.ingredientAdded(ctrl.type)}
+                removed={()=>props.ingredientRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]}
+
+            />
         ))}
+        <button className={classes.OrderButton} disabled={!props.purchasable}>ORDER NOW</button>
     </div>
 );
 
---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,7 @@
+import React from "react";
+import classes from "./Modal.module.css"
+
+const Modal = (props) =>(
+    <div></div>
+)
+export default Modal;
