7d3619bddbaf2087624642b36b6b6c3c1f315a4a
Zeye Gu <zeyegu@gmail.com>
2020-09-04 03:21:28 -0400

    signup signin

---
+++ src/store/actions/auth.js
@@ -0,0 +1,51 @@
+import * as actionTypes from './actionTypes';
+import axios from 'axios';
+
+export const authStart = () => {
+    return {
+        type: actionTypes.AUTH_START
+    }
+}
+
+export const authSuccess = (authData) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        authData:authData
+
+    }
+
+}
+
+
+export const authFailed = (error) => {
+    return{
+        type: actionTypes.AUTH_FAILED,
+        error: error
+    }
+}
+
+
+export const auth = (email,password,isSignup) =>{
+    return dispatch =>{
+
+        dispatch(authStart());
+        const authData={
+            email:email,
+            password:password,
+            returnSecureToken:true
+        }
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAMNKu7ZeZa8gfPd3XOm7wX6ta4NfHlMeI';
+        if(!isSignup){
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAMNKu7ZeZa8gfPd3XOm7wX6ta4NfHlMeI'
+        }
+        axios.post(url,authData)
+            .then(response =>{
+                console.log(response);
+                dispatch(authSuccess(response.data));
+            })
+            .catch(error=>{
+                console.log(error);
+                dispatch(authFailed(error))
+            })
+    }
+}
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,2 +1,3 @@
 export {addIngredient, removeIngredient,initIngredients}from './burgerBuilder';
-export {purchaseBurger,purchaseInit,fetchOrders} from './orderAction'
+export {purchaseBurger,purchaseInit,fetchOrders} from './orderAction';
+export {auth} from './auth';
